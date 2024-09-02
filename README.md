196bf88de315eb7b1070fe39319d3046d59ec918
Harsh4873 <43502626+Harsh4873@users.noreply.github.com>
2024-09-01 19:07:51 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# breakout-Harsh4873
+breakout-Harsh4873 created by GitHub Classroom
+It must (some of these you may have to research to find a solution):
+
+Have a start screen with buttons to start the game. HTML links / buttons would work. Bootstrap has a pretty good set of CSS for buttons.
+Have an end screen when the player loses with buttons allowing them to restart.
+At least 5 levels with different patterns of bricks.
+At least 5 different types of bricks. Ex: plain bricks that break with one hit, metal bricks that take 5, exploding bricks that take out neighboring bricks. These are examples, you can come up with whatever type you want. 
+The game should fill the screen and the window should be a fixed size, no larger than 1024x768, or 768x1024 depending on if you want the game horizontal or vertical. 
+The window should not have a menu.
+Different levels / screens can be separate HTML and JavaScript files or you could use JavaScript to control everything on a single HTML file. 
+
---
+++ renderer.js
@@ -0,0 +1,148 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var size = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -6;
+var paddleHeight = 10;
+var paddleWidth = 100;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+var brickRowCount = 5;
+var brickColumnCount = 3;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var points = 0;
+var chances = 3;
+
+var bricks = [];
+for(var c = 0; c<brickColumnCount; c++) {
+  bricks[c] = [];
+  for(var r = 0; r<brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if(relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth/2;
+  }
+}
+function collisionDetection() {
+  for(var c = 0; c<brickColumnCount; c++) {
+    for(var r = 0; r<brickRowCount; r++) {
+      var b = bricks[c][r];
+      if(b.status == 1) {
+        if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+          dy = -dy;
+          b.status = 0;
+          points++;
+          if(points == brickRowCount*brickColumnCount) {
+            alert("Congrats, You finished Level 1!");
+            window.location.replace("2.html"); 
+          }
+        }
+      }
+    }
+  }
+}
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI*2);
+  ctx.fillStyle = "#ff0000";
+  ctx.fill();
+  ctx.closePath();
+}
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#ff0000";
+  ctx.fill();
+  ctx.closePath();
+}
+function drawBricks() {
+  for(var c = 0; c<brickColumnCount; c++) 
+  {
+    for(var r = 0; r<brickRowCount; r++) 
+    {
+      if(bricks[c][r].status == 1) 
+      {
+        var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+        var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#808080";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
+function drawPoints() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#000000";
+  ctx.fillText("Points: "+points, 8, 20);
+}
+function drawChances() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#000000";
+  ctx.fillText("Chances: "+chances, canvas.width-100, 20);
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBricks();
+  drawBall();
+  drawPaddle();
+  drawPoints();
+  drawChances();
+  collisionDetection();
+
+  if(x + dx > canvas.width-size || x + dx < size) {
+    dx = -dx;
+  }
+  if(y + dy < size) {
+    dy = -dy;
+  }
+  else if(y + dy > canvas.height-size) {
+    if(x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    }
+    else {
+      chances--;
+      if(!chances) {
+        alert("You lost, Press Ok to try again!");
+        document.location.replace("endgame.html");
+      }
+      else {
+        x = canvas.width/2;
+        y = canvas.height-30;
+        paddleX = (canvas.width-paddleWidth)/2;
+      }
+    }
+  }
+
+  if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
+  }
+  else if(leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+
+  x += dx;
+  y += dy;
+  requestAnimationFrame(draw);
+}
+
+draw();
